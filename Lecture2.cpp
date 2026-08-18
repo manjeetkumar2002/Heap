c259a4e8528094d45198795c1f46ad2fcdc73213
Manjeet Kumar <mkj22834@gmail.com>
2026-08-18 20:32:33 +0530

    lecture2 heapSort code added

--- Lecture2.cpp
+++ Lecture2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-// Building maxHeap using setup approach 
+// Building maxHeap using setup approach Time complexity (nlogn)
 void BuildHeapSetupUp(int arr[],int n){
     // You are given a array of element     
     // convert the array into max heap
@@ -16,7 +16,7 @@ void BuildHeapSetupUp(int arr[],int n){
         }
     }
 }
-
+// Building maxHeap using stepdown approach Time complextiy O(n)
 void BuildHeapStepDown(int arr[],int n){
     // start from index (n/2 - 1) because all other have no children
     // place element to its correct position using step down approach
@@ -45,17 +45,54 @@ void BuildHeapStepDown(int arr[],int n){
         
     }
 }
+
+void heapSort(int arr[],int n){
+    //step1 : build the max heap
+    BuildHeapStepDown(arr,n);
+
+    //step2 : heapsort
+    for(int i=n-1;i>0;i--){
+        //step 2 : swap first and last element
+        swap(arr[0],arr[i]);
+        //step 3 : apply heapify on index 0 because it is not in correct position
+        //step down approach
+        int index = 0;
+        int size = i;
+         while(true){
+            int largest = index;
+            int left = 2*index+1;
+            int right = 2*index+2;
+
+            if(left<size&&arr[left]>arr[largest]){
+                largest = left;
+            }
+            if(right<size&&arr[right]>arr[largest]){
+                largest = right;
+            }
+            if(largest==index){
+                break;
+            }
+            
+            swap(arr[largest],arr[index]);
+            index = largest;
+            
+        }
+    }
+   
+}
+
 int main(){
     
     int arr[]={14,15,12,22,17,20,18,24,23,30,19};
     int n = sizeof(arr)/sizeof(int);
     
     // BuildHeapSetupUp(arr,n);
-    BuildHeapStepDown(arr,n);
+    // BuildHeapStepDown(arr,n);
+
+    heapSort(arr,n);
     cout<<"Max Heap : ";
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
-    // Max Heap : 30 24 20 22 23 12 18 14 17 15 19 
 }
